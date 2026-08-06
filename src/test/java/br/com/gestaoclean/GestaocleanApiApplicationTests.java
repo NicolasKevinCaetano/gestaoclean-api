a68129006436139f87df0dce72c2387fd92ce614
NicolasKevinCaetano <nicolaskevinoliveira@gmail.com>
2026-08-06 10:19:10 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/br/com/gestaoclean/GestaocleanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaocleanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
